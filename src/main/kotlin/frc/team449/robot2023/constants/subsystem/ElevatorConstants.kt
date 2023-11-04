cb1007807e9b00cb2aa8e748b10eaba436635489
Jeffery Ji <jefferydoudou@gmail.com>
2023-11-04 12:01:30 -0400

    Created Elevator Subsystem

---
+++ src/main/kotlin/frc/team449/robot2023/constants/subsystem/ElevatorConstants.kt
@@ -0,0 +1,27 @@
+package frc.team449.robot2023.constants.subsystem
+
+object ElevatorConstants {
+
+  const val LEFT_ID = 1
+  const val LEFT_INVERTED = false
+  const val RIGHT_ID = 2
+  const val RIGHT_INVERTED = true
+  const val GEARING = 1.0
+  const val UPR = 1.0
+
+  const val POS_TOL = 0.1
+  const val VEL_TOL = 0.1
+
+  const val MAX_VEL = 1.0
+  const val MAX_ACC = 1.0
+
+  const val KP = 1.0
+  const val KI = 0.0
+  const val KD = 0.0
+
+  const val KS = 0.0
+  const val KV = 0.0
+  const val KA = 0.0
+
+  const val EXTEND_DISTANCE = 1.0
+}
